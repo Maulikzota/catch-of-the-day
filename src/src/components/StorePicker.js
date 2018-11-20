8fa32c079875923b1e01641be238f39af7b31f75
Maulik Zota <mmz9731@rit.edu>
2018-11-20 16:01:34 -0500

    Updating StorePicker.js

--- src/src/components/StorePicker.js
+++ src/src/components/StorePicker.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { getFunName } from '../helpers'; 
+import PropTypes from 'prop-types';
 
 class StorePicker extends React.Component{
+	// constructor(){
+	// 	super();
+	// 	this.getToSotore = this.getToSotore.bind(this);
+	// }
+
+	getToSotore(event){
+		event.preventDefaults();
+		const storeId = this.storeInput.value;
+		this.context.router.history.push('/store/${storeId}');
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.getToSotore(e)}> 
+			{/*<form className="store-selector" onSubmit={this.getToSotore}> */}
 				{/* This is a comment section */}
 				<h2> Please Enter A Store </h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
 				<button type="submit"> Visit Store -> </button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes
 }
 
 export default StorePicker;
